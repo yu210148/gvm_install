3bb6d6f6f1f2b03474be546f3e370522b9d63a2d
Kevin Lucas <yu210148@gmail.com>
2020-12-07 10:07:01 -0500

    testing

---
+++ step15.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# step 15
+
+# Create GVM Scanner
+su gvm -c "touch /opt/gvm/scan.sh"
+su gvm -c "chmod u+x /opt/gvm/scan.sh"
+sudo -Hiu gvm echo -e "/opt/gvm/sbin/gvmd --create-scanner=\"Created OpenVAS Scanner\" --scanner-type=\"OpenVAS\" --scanner-host=/opt/gvm/var/run/ospd.sock" | sudo -Hiu gvm tee -a /opt/gvm/.bashrc
+
+sudo -Hiu gvm echo "/opt/gvm/sbin/gvmd --get-scanners" | sudo -Hiu gvm tee -a /opt/gvm/.bashrc
+
+# Verify newly created scanner
+sudo -Hiu gvm echo -e "UUID=\$(gvmd --get-scanners | grep Created | awk '{print \$\1}')" | sed 's/\\//g' | sudo -Hiu gvm tee -a /opt/gvm/.bashrc
+
+# Wait a moment then verify the scanner
+sudo -Hiu gvm echo "sleep 10" | sudo -Hiu gvm tee -a /opt/gvm/.bashrc
+sudo -Hiu gvm echo -e "/opt/gvm/sbin/gvmd --verify-scanner=UUID" | sed 's/UUID/\$UUID/g' | sudo -Hiu gvm tee -a /opt/gvm/.bashrc
