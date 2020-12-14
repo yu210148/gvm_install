2ea775e3b5f6694e36eeb81266c32b1a84558e58
Kevin Lucas <yu210148@gmail.com>
2020-12-14 14:19:29 -0500

    testing

--- step7.sh
+++ step7.sh
@@ -14,8 +14,8 @@ if [[ $ID = "debian" ]] || [[ $ID = "kali" ]]; then
 else
     sudo -Hiu gvm echo "cmake .. -DCMAKE_INSTALL_PREFIX=/opt/gvm" | sudo -Hiu gvm tee -a /opt/gvm/gsa_build.sh
 fi
-sudo -Hiu gvm echo "#make" | sudo -Hiu gvm tee -a /opt/gvm/gsa_build.sh
-sudo -Hiu gvm echo "#make install" | sudo -Hiu gvm tee -a /opt/gvm/gsa_build.sh
+sudo -Hiu gvm echo "make" | sudo -Hiu gvm tee -a /opt/gvm/gsa_build.sh
+sudo -Hiu gvm echo "make install" | sudo -Hiu gvm tee -a /opt/gvm/gsa_build.sh
 
-su gvm -c "/opt/gvm/gsa_build.sh"
-su gvm -c "rm /opt/gvm/gsa_build.sh"
+#su gvm -c "/opt/gvm/gsa_build.sh"
+#su gvm -c "rm /opt/gvm/gsa_build.sh"
