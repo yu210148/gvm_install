a1c3c3b453cce955d63402d853e31125cfa1ccd6
Kevin Lucas <yu210148@gmail.com>
2020-12-14 11:02:15 -0500

    stop ID_LIKE value being included

--- install_gvm.sh
+++ install_gvm.sh
@@ -37,7 +37,7 @@ systemctl enable postgresql
 
 # Kali Linux uses postgresql 13 which cmake doesn't know about as of version 3.18 so it get's added here
 # should have no effect on Debian stable as the line starts with "11" rather than "12" so it won't be matched
-ID=`grep ^ID /etc/os-release | sed 's/ID=//g'`
+ID=`grep ^ID= /etc/os-release | sed 's/ID=//g'`
 if [ $ID = "debian" ] || [$ID = "kali"]; then
     sed -i 's/"12" "11" "10"/"13" "12" "11" "10"/g' /usr/share/cmake-3.18/Modules/FindPostgreSQL.cmake
 fi
@@ -123,7 +123,7 @@ systemctl start redis-server@openvas
 systemctl enable redis-server@openvas
 echo "gvm ALL = NOPASSWD: /opt/gvm/sbin/openvas" > /etc/sudoers.d/gvm
 # This next line varies between Debian and Ubuntu because it includes /snap/bin on Ubuntu                                                                                                    
-ID=`grep ^ID /etc/os-release | sed 's/ID=//g'`
+ID=`grep ^ID= /etc/os-release | sed 's/ID=//g'`
 if [ $ID = "debian" ] || [$ID = "kali"]; then
     sed 's/Defaults\s.*secure_path=\"\/usr\/local\/sbin:\/usr\/local\/bin:\/usr\/sbin:\/usr\/bin:\/sbin:\/bin"/Defaults secure_path=\"\/usr\/local\/sbin:\/usr\/local\/bin:\/usr\/sbin:\/usr\/bin:\/sbin:\/bin:\/opt\/gvm\/sbin\:\/opt\/gvm\/bin"/g' /etc/sudoers | EDITOR='tee' visudo
     
@@ -195,7 +195,7 @@ sudo -Hiu gvm echo "cd /tmp/gvm-source/gsa" | sudo -Hiu gvm tee -a /opt/gvm/gsa_
 sudo -Hiu gvm echo "mkdir build" | sudo -Hiu gvm tee -a /opt/gvm/gsa_build.sh
 sudo -Hiu gvm echo "cd build" | sudo -Hiu gvm tee -a /opt/gvm/gsa_build.sh
 # if debian { cmake .. -DCMAKE_INSTALL_PREFIX=/opt/gvm -DCMAKE_BUILD_TYPE=RELEASE }
-ID=`grep ^ID /etc/os-release | sed 's/ID=//g'`
+ID=`grep ^ID= /etc/os-release | sed 's/ID=//g'`
 if [ $ID = "debian" ] || [$ID = "kali"]; then
     sudo -Hiu gvm echo "cmake .. -DCMAKE_INSTALL_PREFIX=/opt/gvm -DCMAKE_BUILD_TYPE=RELEASE" | sudo -Hiu gvm tee -a /opt/gvm/gsa_build
 else
@@ -266,7 +266,7 @@ sudo -Hiu gvm echo "export PKG_CONFIG_PATH=/opt/gvm/lib/pkgconfig:$PKG_CONFIG_PA
 
 # another difference here between Ubuntu and Debian
 # Debian needs the below to be 'python3.7' while Ubuntu 'python3.8'
-ID=`grep ^ID /etc/os-release | sed 's/ID=//g'`
+ID=`grep ^ID= /etc/os-release | sed 's/ID=//g'`
 if [ $ID = "debian" ] || [$ID = "kali"]; then
     sudo -Hiu gvm echo "mkdir -p /opt/gvm/lib/python3.7/site-packages/" | sudo -Hiu gvm tee -a /opt/gvm/ospd.sh
     sudo -Hiu gvm echo "export PYTHONPATH=/opt/gvm/lib/python3.7/site-packages" | sudo -Hiu gvm tee -a /opt/gvm/ospd.sh
@@ -289,7 +289,7 @@ su gvm -c "rm /opt/gvm/ospd.sh"
 su gvm -c "touch /opt/gvm/start.sh"
 su gvm -c "chmod u+x /opt/gvm/start.sh"
 
-ID=`grep ^ID /etc/os-release | sed 's/ID=//g'`
+ID=`grep ^ID= /etc/os-release | sed 's/ID=//g'`
 if [ $ID = "debian" ] || [$ID = "kali"]; then
     sudo -Hiu gvm echo "export PYTHONPATH=/opt/gvm/lib/python3.7/site-packages" | sudo -Hiu gvm tee -a /opt/gvm/start.sh
 else
