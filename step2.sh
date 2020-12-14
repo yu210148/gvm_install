8f4ea0e852f9a422f95253de68b4f8d225565c4a
Kevin Lucas <yu210148@gmail.com>
2020-12-14 11:09:47 -0500

    testing

--- step2.sh
+++ step2.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-# This next line varies between Debian and Ubuntu because it includes /snap/bin on Ubuntu                                                                                                    
-ID=`grep ^ID /etc/os-release | sed 's/ID=//g'`
-
-if [ $ID = "debian" ]; then
-    sed 's/Defaults\s.*secure_path=\"\/usr\/local\/sbin:\/usr\/local\/bin:\/usr\/sbin:\/usr\/bin:\/sbin:\/bin"/Defaults secure_path=\"\/usr\/local\/sbin:\/usr\/local\/bin:\/usr\/sbin:\/usr\/bin:\/sbin:\/bin:\/opt\/gvm\/sbin\:\/opt\/gvm\/bin"/g' /etc/sudoers | EDITOR='tee' visudo
-else
-    sed 's/Defaults\s.*secure_path=\"\/usr\/local\/sbin:\/usr\/local\/bin:\/usr\/sbin:\/usr\/bin:\/sbin:\/bin:\/snap\/bin\"/Defaults secure_path=\"\/usr\/local\/sbin:\/usr\/local\/bin:\/usr\/sbin:\/usr\/bin:\/sbin:\/bin:\/snap\/bin:\/opt\/gvm\/sbin\"/g' /etc/sudoers | EDITOR='tee' visudo
+systemctl restart postgresql
+ID=`grep ^ID= /etc/os-release | sed 's/ID=//g'`
+if [ $ID = "debian" ] || [$ID = "kali"]; then
+    sudo -Hiu postgres touch ~/.hushlogin
 fi
+sudo -Hiu postgres createuser gvm
+sudo -Hiu postgres createdb -O gvm gvmd
+sudo -Hiu postgres psql -c 'create role dba with superuser noinherit;' gvmd
+sudo -Hiu postgres psql -c 'grant dba to gvm;' gvmd
+sudo -Hiu postgres psql -c 'create extension "uuid-ossp";' gvmd
+systemctl restart postgresql
+systemctl enable postgresql
