dd248e7e7c47f7d28cb44dac3e0582c48c5f405d
Kevin Lucas <yu210148@gmail.com>
2020-12-14 13:13:05 -0500

    testing

--- step6.sh
+++ step6.sh
@@ -1,26 +1,15 @@
 #!/bin/bash
+# Build and Install Greenbone Vulnerability Manager
+su gvm -c "touch /opt/gvm/gvm_build.sh"
+su gvm -c "chmod u+x /opt/gvm/gvm_build.sh"
 
+sudo -Hiu gvm echo "export PKG_CONFIG_PATH=/opt/gvm/lib/pkgconfig:$PKG_CONFIG_PATH" | sudo -Hiu gvm tee -a /opt/gvm/gvm_build.sh
+sudo -Hiu gvm echo "cd /tmp/gvm-source/gvmd" | sudo -Hiu gvm tee -a /opt/gvm/gvm_build.sh
+sudo -Hiu gvm echo "mkdir build" | sudo -Hiu gvm tee -a /opt/gvm/gvm_build.sh
+sudo -Hiu gvm echo "cd build" | sudo -Hiu gvm tee -a /opt/gvm/gvm_build.sh
+sudo -Hiu gvm echo "cmake .. -DCMAKE_INSTALL_PREFIX=/opt/gvm" | sudo -Hiu gvm tee -a /opt/gvm/gvm_build.sh
+sudo -Hiu gvm echo "make" | sudo -Hiu gvm tee -a /opt/gvm/gvm_build.sh
+sudo -Hiu gvm echo "make install" | sudo -Hiu gvm tee -a /opt/gvm/gvm_build.sh
 
-# This next command fails in get_community_feed function in greenbone-nvt-sync if the
-# rsync calls are too close together as only one connection is allowed at a time. So we
-# need to add a sleep command in that file to pause the sync so that the NAT connection can close
-# file is in /opt/gvm/bin and the line to edit is 364. More info can be found by searching
-# greenbone-nvt-sync rsync connection refused
-#
-# add in the following
-#  # sleep to allow NAT connection to close                                                                                                                                                   
-#  sleep 300
-sudo -Hiu gvm echo "sed -i '364isleep 300' /opt/gvm/bin/greenbone-nvt-sync" | sudo -Hiu gvm tee -a /opt/gvm/.bashrc
-sudo -Hiu gvm echo "echo 'Sleeping for 5 minutes'" | sudo -Hiu gvm tee -a /opt/gvm/.bashrc
-sudo -Hiu gvm echo "echo 'More info can be found by searching greenbone-nvt-sync rsync connection refused on Google'" | sudo -Hiu gvm tee -a /opt/gvm/.bashrc
-#sudo -Hiu gvm echo "greenbone-nvt-sync" | sudo -Hiu gvm tee -a /opt/gvm/.bashrc
-
-
-#sudo -Hiu gvm echo "sudo openvas --update-vt-info" | sudo -Hiu gvm tee -a /opt/gvm/.bashrc
-
-# Leave gvm environment and clean up
-sudo -Hiu gvm echo "exit" | sudo -Hiu gvm tee -a /opt/gvm/.bashrc
-su gvm
-
-sudo -Hiu gvm rm /opt/gvm/.bashrc
-sudo -Hiu gvm mv /opt/gvm/.bashrc.bak /opt/gvm/.bashrc
+su gvm -c "/opt/gvm/gvm_build.sh"
+su gvm -c "rm /opt/gvm/gvm_build.sh"
