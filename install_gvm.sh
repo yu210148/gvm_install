4e4614889b9902ae9f42381ac1245dc899abc909
Kevin Lucas <yu210148@gmail.com>
2020-12-15 10:10:08 -0500

    use python3 version

--- install_gvm.sh
+++ install_gvm.sh
@@ -272,27 +272,33 @@ su gvm -c "rm /opt/gvm/cron.sh"
 # step 10 below
 
 # Build and Install OSPd and OSPd-OpenVAS
-# Build and Install OSPd and OSPd-OpenVAS
 su gvm -c "touch /opt/gvm/ospd.sh"
 su gvm -c "chmod u+x /opt/gvm/ospd.sh"
 
 sudo -Hiu gvm echo "export PKG_CONFIG_PATH=/opt/gvm/lib/pkgconfig:$PKG_CONFIG_PATH" | sudo -Hiu gvm tee -a /opt/gvm/ospd.sh
 
 # another difference here between Ubuntu and Debian
-# Debian needs the below to be 'python3.7' while Ubuntu 'python3.8'; kali seems to use python3.9
-#
-# a better way to do this would be to pull the python 3 version number directly. e.g., PY3VER=`python3 --version | grep -o [0-9]\.[0-9]`
-ID=`grep ^ID= /etc/os-release | sed 's/ID=//g'`
-if [ $ID = "debian" ]; then
-    sudo -Hiu gvm echo "mkdir -p /opt/gvm/lib/python3.7/site-packages/" | sudo -Hiu gvm tee -a /opt/gvm/ospd.sh
-    sudo -Hiu gvm echo "export PYTHONPATH=/opt/gvm/lib/python3.7/site-packages" | sudo -Hiu gvm tee -a /opt/gvm/ospd.sh
-elif [ $ID = "kali" ]; then
-    sudo -Hiu gvm echo "mkdir -p /opt/gvm/lib/python3.9/site-packages/" | sudo -Hiu gvm tee -a /opt/gvm/ospd.sh
-    sudo -Hiu gvm echo "export PYTHONPATH=/opt/gvm/lib/python3.9/site-packages" | sudo -Hiu gvm tee -a /opt/gvm/ospd.sh
-else
-    sudo -Hiu gvm echo "mkdir -p /opt/gvm/lib/python3.8/site-packages/" | sudo -Hiu gvm tee -a /opt/gvm/ospd.sh
-    sudo -Hiu gvm echo "export PYTHONPATH=/opt/gvm/lib/python3.8/site-packages" | sudo -Hiu gvm tee -a /opt/gvm/ospd.sh
-fi
+# Debian needs the below to be 'python3.7' while Ubuntu 'python3.8'
+
+# going to just get the python3 version number and use it here. That should be better than trying
+# to account for the differences with the release ID
+
+PY3VER=`python3 --version | grep -o [0-9]\.[0-9]`
+sudo -Hiu gvm echo "mkdir -p /opt/gvm/lib/python$PY3VER/site-packages/" | sudo -Hiu gvm tee -a /opt/gvm/ospd.sh
+sudo -Hiu gvm echo "export PYTHONPATH=/opt/gvm/lib/python$PY3VER/site-packages" | sudo -Hiu gvm tee -a /opt/gvm/ospd.sh
+
+
+#ID=`grep ^ID= /etc/os-release | sed 's/ID=//g'`
+#if [ $ID = "debian" ]; then
+#    sudo -Hiu gvm echo "mkdir -p /opt/gvm/lib/python3.7/site-packages/" | sudo -Hiu gvm tee -a /opt/gvm/ospd.sh
+#    sudo -Hiu gvm echo "export PYTHONPATH=/opt/gvm/lib/python3.7/site-packages" | sudo -Hiu gvm tee -a /opt/gvm/ospd.sh
+#elif [ $ID = "kali" ]; then
+#    sudo -Hiu gvm echo "mkdir -p /opt/gvm/lib/python3.9/site-packages/" | sudo -Hiu gvm tee -a /opt/gvm/ospd.sh
+#    sudo -Hiu gvm echo "export PYTHONPATH=/opt/gvm/lib/python3.9/site-packages" | sudo -Hiu gvm tee -a /opt/gvm/ospd.sh
+#else
+#    sudo -Hiu gvm echo "mkdir -p /opt/gvm/lib/python3.8/site-packages/" | sudo -Hiu gvm tee -a /opt/gvm/ospd.sh
+#    sudo -Hiu gvm echo "export PYTHONPATH=/opt/gvm/lib/python3.8/site-packages" | sudo -Hiu gvm tee -a /opt/gvm/ospd.sh
+#fi
 sudo -Hiu gvm echo "cd /tmp/gvm-source/ospd" | sudo -Hiu gvm tee -a /opt/gvm/ospd.sh
 sudo -Hiu gvm echo "python3 setup.py install --prefix=/opt/gvm" | sudo -Hiu gvm tee -a /opt/gvm/ospd.sh
 sudo -Hiu gvm echo "cd ../ospd-openvas" | sudo -Hiu gvm tee -a /opt/gvm/ospd.sh
