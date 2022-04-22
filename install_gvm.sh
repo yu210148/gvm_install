615ea17921d4430c56e69b02bcf217175f9bc0fe
rybli <ryanblitch93@gmail.com>
2022-04-22 12:02:44 -0700

    Logic added to check gvm user has home/shell set.

--- install_gvm.sh
+++ install_gvm.sh
@@ -17,6 +17,8 @@
 ###################################
 UFW=false
 API=false
+gvm_home_dir="$cat /etc/passwd | grep gvm | cut -f6 -d:"
+gvm_shell="$cat /etc/passwd | grep gvm | cut -f7 -d:"
 
 ###################################
 # SCRIPT FUNCTIONS
@@ -97,6 +99,19 @@ fi
 apt-get update
 apt-get upgrade -y 
 useradd -r -d /opt/gvm -c "GVM (OpenVAS) User" -s /bin/bash gvm
+# Check if gvm users has the correct home directory set.
+# This issue has been seen in AWS EC2 instances. Issue #74
+if ! [ "$gvm_home_dir" = "/opt/gvm" ]
+then
+    usermod -d /opt/gvm gvm
+fi
+
+# Check if gvm user has the correct shell set.
+# This issue has been seen in AWS Ec2 instances. Issue #74
+if ! [ "$gvm_shell" = "/bin/bash" ]
+then
+    usermod -s /bin/bash gvm
+fi
 mkdir /opt/gvm
 chown gvm:gvm /opt/gvm
 apt-get -y install gcc g++ make bison flex libksba-dev curl redis libpcap-dev cmake git pkg-config libglib2.0-dev libgpgme-dev libgnutls28-dev uuid-dev libssh-gcrypt-dev libldap2-dev gnutls-bin libmicrohttpd-dev libhiredis-dev zlib1g-dev libxml2-dev libradcli-dev clang-format libldap2-dev doxygen nmap gcc-mingw-w64 xml-twig-tools libical-dev perl-base heimdal-dev libpopt-dev libsnmp-dev python3-setuptools python3-paramiko python3-lxml python3-defusedxml python3-dev gettext python3-polib xmltoman python3-pip texlive-fonts-recommended xsltproc texlive-latex-extra rsync ufw ntp libunistring-dev git libnet1-dev graphviz graphviz-dev --no-install-recommends
