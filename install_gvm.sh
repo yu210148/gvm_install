074c37d486f78c6dc5cf7d59831f7c2d100071a3
itiligent <94789708+itiligent@users.noreply.github.com>
2022-07-07 15:07:53 +1000

    removed tmp mount
    Not using this. Creates errors on install with Ubuntu defaut partioning.

--- install_gvm.sh
+++ install_gvm.sh
@@ -96,15 +96,6 @@ else
     exit 1
 fi
 
-TMP_IS_MOUNTED=$(df |grep /tmp$|wc -l)
-
-if [ $TMP_IS_MOUNTED -eq 0 ]; then
-	echo "Re-mounting partition /tmp"
-	sleep 3
-	sudo mount -o remount,defaults /tmp
-	echo "After installation, check if your partition /tmp is mounted correctly"
-fi
-
 apt-get update
 apt-get upgrade -y 
 useradd -r -d /opt/gvm -c "GVM (OpenVAS) User" -s /bin/bash gvm
